337d94ee4e36826b94fc60e1c8f8a98967b29414
Patricia Ann Comendador <patriciaann.c10@gmail.com>
2026-06-03 16:58:18 +0200

    added esercizio 6

---
+++ esercizio 6/README.md
@@ -0,0 +1,7 @@
+# Esercizio 6
+Chiedi all'utente una parola e un numero N. Stampa quella parola N volte.
+
+## Codifica
+``` js
+
+```
---
+++ esercizio 6/main.js
@@ -0,0 +1,6 @@
+let parola = prompt("Inserisci una parola");
+let N = parseInt(prompt("Inserisci un numero"));
+
+for (let i = 0; i < N; i++) {
+    console.log(parola);
+}
